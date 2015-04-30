e96450a1901d510bcc0efa7930759312af2dc236
Lotheren <lotheren@gmail.com>
2015-04-30 14:21:37 -0700

    Index.php change

--- similarResults.php
+++ similarResults.php
@@ -9,37 +9,43 @@
 
 <?php
 
+function fisherYates(&$array)
+{
+    for ($i = count($array) - 1; $i > 0; $i--)
+    {
+        $j = rand(0, $i);
+        $tmp = $array[$i];
+        $array[$i] = $array[$j];
+        $array[$j] = $tmp;
+    }
+}
+
 function outputSimilarResults($result)
 {
-	
-	       
-          
-	//$counter=0;
-		//$counter = $counter+1;
-  //$to_be_recovered_name=$_POST['searchname'];
-  //$sql = "SELECT * FROM education where title ='$to_be_recovered_name'";
-  //$counter = 0;
-  //print $counter;
+
   if ($result)
   {
-	  //and $counter<8
-        while ($row = mysqli_fetch_array($result) )
-        {
-	  	
-        $title =  $row["title"];
-	    $temp = $row["lesson_link"];
-		$link = $row["lesson_image"];
-		
-		print "
-		<div id=activtyIconSec class=col-md-12>
-		<a href=$temp><img src=$link id=activityIconImageSimilar>	</a>
-		<p id=activityTextSimilar>$title</p>		
-		</div>
-        ";
-	    //<a href=$temp style=text-decoration:none>
-	    //$counter=$counter+1;
-        }
-        mysqli_free_result($result);
-      }
+    $generatedHTML;
+    $i = 0;
+    while ($row = mysqli_fetch_array($result) )
+    {
+      $title =  $row["title"];
+      $temp = $row["lesson_link"];
+      $link = $row["lesson_image"];
+    
+      $generatedHTML[$i] = "
+    <div id=activtyIconSec class=col-md-12>
+    <a href=$temp><img src=$link id=activityIconImageSimilar>  </a>
+    <p id=activityTextSimilar>$title</p>    
+    </div>
+      ";
+      $i++;
+    }
+    mysqli_free_result($result);
+    fisherYates($generatedHTML);
+    for ($i = 0; $i < count($generatedHTML); $i++) {
+      print $generatedHTML[$i];
+    }
+  }
 }
 ?>
